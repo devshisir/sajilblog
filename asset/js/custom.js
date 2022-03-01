9e1acb8e01f93bab530820c4b3a75a900c688d56
devshisir <contact.shisirimran@gmail.com>
2022-03-01 14:50:57 +0600

    Done desktop version

--- asset/js/custom.js
+++ asset/js/custom.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
     var school = $(window);
     var page = $('html, body');
+
+    // info box toggle 
+    $('#open__info__box').on('click',function(){
+        $(this).addClass('toggle');
+        $('#info__box').toggleClass('show');
+    })
+    $('#info__box__close').on('click',function(){
+        $('#open__info__box').removeClass('toggle');
+        $('#info__box').toggleClass('show');
+    })
+    
     
 });
